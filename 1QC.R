d3605d7b1bae7e32dfb371fdc51043ea4a5bff35
panpanbai6-hue <panpanbai6@gmail.com>
2026-07-23 17:16:27 +0800

    Update 1QC.R

--- 1QC.R
+++ 1QC.R
@@ -38,11 +38,12 @@ samples <- c(
 
 sample_dirs <- file.path(matrix_dir, samples)
 
-mt_pattern <- "^soybean-mt-"
-cp_pattern <- "^soybean-cp-"
+mt_pattern <- "soybean-mt-"
+cp_pattern <- "soybean-cp-"
 
 ###############################################################################
-## 3. Read 10X matrices and create Seurat objects
+## 3. Read 10X matrices, create Seurat objects,
+##    and calculate mitochondrial and chloroplast proportions
 ###############################################################################
 
 scn.all <- vector("list", length(samples))
@@ -56,14 +57,28 @@ for (i in seq_along(samples)) {
     data.dir = sample_dirs[i]
   )
   
-  scn.all[[i]] <- CreateSeuratObject(
+  obj <- CreateSeuratObject(
     counts = counts,
     min.cells = 5,
     min.features = 300,
     project = samples[i]
   )
   
-  scn.all[[i]]$library <- samples[i]
+  obj$library <- samples[i]
+  
+  obj[["percent.mt"]] <- PercentageFeatureSet(
+    obj,
+    pattern = mt_pattern
+  )
+  
+  obj[["percent.cp"]] <- PercentageFeatureSet(
+    obj,
+    pattern = cp_pattern
+  )
+  
+  obj$percent.organelle <- obj$percent.mt + obj$percent.cp
+  
+  scn.all[[i]] <- obj
 }
 
 raw_cell_number <- sapply(
@@ -72,7 +87,8 @@ raw_cell_number <- sapply(
 )
 
 ###############################################################################
-## 4. Inspect raw nCount_RNA and nFeature_RNA before filtering
+## 4. Inspect raw nCount_RNA, nFeature_RNA,
+##    mitochondrial and chloroplast proportions before filtering
 ###############################################################################
 
 scn_combine_raw <- merge(
@@ -83,16 +99,22 @@ scn_combine_raw <- merge(
 
 p <- VlnPlot(
   scn_combine_raw,
-  features = c("nFeature_RNA", "nCount_RNA"),
+  features = c(
+    "nFeature_RNA",
+    "nCount_RNA",
+    "percent.mt",
+    "percent.cp",
+    "percent.organelle"
+  ),
   group.by = "orig.ident",
-  ncol = 2,
+  ncol = 5,
   pt.size = 0
 )
 
 ggsave(
-  filename = file.path(output_dir, "scn_combine_before_cell_filter.pdf"),
+  filename = file.path(output_dir, "scn_combine_before_cell_filter_qc.pdf"),
   plot = p,
-  width = 20,
+  width = 30,
   height = 6
 )
 
@@ -138,21 +160,6 @@ scn_combine_basic_filter <- merge(
   add.cell.ids = samples
 )
 
-p <- VlnPlot(
-  scn_combine_basic_filter,
-  features = c("nFeature_RNA", "nCount_RNA"),
-  group.by = "orig.ident",
-  ncol = 2,
-  pt.size = 0
-)
-
-ggsave(
-  filename = file.path(output_dir, "scn_combine_after_nCount_nFeature_filter.pdf"),
-  plot = p,
-  width = 20,
-  height = 6
-)
-
 rm(scn_combine_basic_filter)
 
 ###############################################################################
@@ -184,13 +191,20 @@ for (i in seq_along(samples)) {
     verbose = FALSE
   )
   
-  obj <- ScaleData(obj)
+  obj <- ScaleData(
+    obj,
+    verbose = FALSE
+  )
   
-  obj <- RunPCA(obj)
+  obj <- RunPCA(
+    obj,
+    verbose = FALSE
+  )
   
   obj <- FindNeighbors(
     obj,
-    dims = 1:20
+    dims = 1:20,
+    verbose = FALSE
   )
   
   obj <- FindClusters(
@@ -201,7 +215,8 @@ for (i in seq_along(samples)) {
   
   obj <- RunUMAP(
     obj,
-    dims = 1:20
+    dims = 1:20,
+    verbose = FALSE
   )
   
   sweep.res_list <- paramSweep(
@@ -221,14 +236,14 @@ for (i in seq_along(samples)) {
   
   pK_use <- as.numeric(
     as.character(
-      bcmvn$pK[which.max(bcmvn$MeanBC)]
+      bcmvn$pK[
+        which.max(bcmvn$MeanBC)
+      ]
     )
   )
   
-  annotations <- obj$seurat_clusters
-  
   homotypic.prop <- modelHomotypic(
-    annotations
+    obj$seurat_clusters
   )
   
   nExp_poi <- round(
@@ -266,28 +281,6 @@ for (i in seq_along(samples)) {
   
   obj$doublet_info <- obj@meta.data[[doublet_cols[length(doublet_cols)]]]
   
-  doublet_table <- as.data.frame(
-    table(obj$doublet_info)
-  )
-  
-  colnames(doublet_table) <- c("doublet_info", "n_cells")
-  
-  n_singlet <- doublet_table$n_cells[
-    doublet_table$doublet_info == "Singlet"
-  ]
-  
-  n_doublet <- doublet_table$n_cells[
-    doublet_table$doublet_info == "Doublet"
-  ]
-  
-  if (length(n_singlet) == 0) {
-    n_singlet <- 0
-  }
-  
-  if (length(n_doublet) == 0) {
-    n_doublet <- 0
-  }
-  
   doublet_filter_summary[[i]] <- data.frame(
     sample = samples[i],
     cells_before_doublet_filter = ncol(obj),
@@ -296,8 +289,8 @@ for (i in seq_along(samples)) {
     nExp_poi = nExp_poi,
     homotypic_prop = homotypic.prop,
     nExp_poi_adjusted = nExp_poi.adj,
-    n_singlet = n_singlet,
-    n_doublet = n_doublet
+    n_singlet = sum(obj$doublet_info == "Singlet"),
+    n_doublet = sum(obj$doublet_info == "Doublet")
   )
   
   obj <- subset(
@@ -319,14 +312,11 @@ for (i in seq_along(samples)) {
     doublet_cols
   )
   
-  if (length(remove_cols) > 0) {
-    
-    obj@meta.data <- obj@meta.data[
-      ,
-      !colnames(obj@meta.data) %in% remove_cols,
-      drop = FALSE
-    ]
-  }
+  obj@meta.data <- obj@meta.data[
+    ,
+    !colnames(obj@meta.data) %in% remove_cols,
+    drop = FALSE
+  ]
   
   scn.all.filterdouble[[i]] <- obj
 }
@@ -347,122 +337,7 @@ cell_number_after_doublet <- sapply(
 )
 
 ###############################################################################
-## 7. Calculate mitochondrial and chloroplast proportions
-###############################################################################
-
-organelle_qc_after_doublet <- vector("list", length(samples))
-
-for (i in seq_along(samples)) {
-  
-  message("Calculating organelle proportions for sample: ", samples[i])
-  
-  mt_features <- rownames(scn.all.filterdouble[[i]])[
-    grepl(
-      mt_pattern,
-      rownames(scn.all.filterdouble[[i]]),
-      ignore.case = TRUE
-    )
-  ]
-  
-  cp_features <- rownames(scn.all.filterdouble[[i]])[
-    grepl(
-      cp_pattern,
-      rownames(scn.all.filterdouble[[i]]),
-      ignore.case = TRUE
-    )
-  ]
-  
-  mt_features <- intersect(
-    mt_features,
-    rownames(scn.all.filterdouble[[i]])
-  )
-  
-  cp_features <- intersect(
-    cp_features,
-    rownames(scn.all.filterdouble[[i]])
-  )
-  
-  if (length(mt_features) > 0) {
-    
-    scn.all.filterdouble[[i]][["percent.mt"]] <- PercentageFeatureSet(
-      scn.all.filterdouble[[i]],
-      features = mt_features
-    )
-    
-  } else {
-    
-    scn.all.filterdouble[[i]]$percent.mt <- 0
-  }
-  
-  if (length(cp_features) > 0) {
-    
-    scn.all.filterdouble[[i]][["percent.cp"]] <- PercentageFeatureSet(
-      scn.all.filterdouble[[i]],
-      features = cp_features
-    )
-    
-  } else {
-    
-    scn.all.filterdouble[[i]]$percent.cp <- 0
-  }
-  
-  scn.all.filterdouble[[i]]$percent.organelle <-
-    scn.all.filterdouble[[i]]$percent.mt +
-    scn.all.filterdouble[[i]]$percent.cp
-  
-  organelle_qc_after_doublet[[i]] <- data.frame(
-    sample = samples[i],
-    n_cells_after_doublet = ncol(scn.all.filterdouble[[i]]),
-    n_mt_genes = length(mt_features),
-    n_cp_genes = length(cp_features),
-    median_percent_mt = median(scn.all.filterdouble[[i]]$percent.mt),
-    median_percent_cp = median(scn.all.filterdouble[[i]]$percent.cp),
-    median_percent_organelle = median(scn.all.filterdouble[[i]]$percent.organelle),
-    max_percent_organelle = max(scn.all.filterdouble[[i]]$percent.organelle)
-  )
-}
-
-organelle_qc_after_doublet <- bind_rows(
-  organelle_qc_after_doublet
-)
-
-write.csv(
-  organelle_qc_after_doublet,
-  file = file.path(output_dir, "organelle_percent_summary_after_doublet_filter.csv"),
-  row.names = FALSE
-)
-
-scn_combine_after_doublet <- merge(
-  x = scn.all.filterdouble[[1]],
-  y = scn.all.filterdouble[-1],
-  add.cell.ids = samples
-)
-
-p <- VlnPlot(
-  scn_combine_after_doublet,
-  features = c(
-    "nFeature_RNA",
-    "nCount_RNA",
-    "percent.mt",
-    "percent.cp",
-    "percent.organelle"
-  ),
-  group.by = "orig.ident",
-  ncol = 5,
-  pt.size = 0
-)
-
-ggsave(
-  filename = file.path(output_dir, "qc_after_nCount_nFeature_and_doublet_filter.pdf"),
-  plot = p,
-  width = 30,
-  height = 6
-)
-
-rm(scn_combine_after_doublet)
-
-###############################################################################
-## 8. Classify soybean and SCN nuclei, then remove organelle genes
+## 7. Classify soybean and SCN nuclei, then remove organelle genes
 ###############################################################################
 
 scn_combine_filter_species <- vector("list", length(samples))
@@ -496,12 +371,20 @@ for (i in seq_along(samples)) {
     table(obj$species)
   )
   
-  colnames(species_table) <- c("species", "n_cells")
+  colnames(species_table) <- c(
+    "species",
+    "n_cells"
+  )
+  
   species_table$sample <- samples[i]
   
   species_classification_summary[[i]] <- species_table[
     ,
-    c("sample", "species", "n_cells")
+    c(
+      "sample",
+      "species",
+      "n_cells"
+    )
   ]
   
   all_genes <- rownames(obj)
@@ -520,12 +403,18 @@ for (i in seq_along(samples)) {
   ]
   
   soybean_genes <- all_genes[
-    grepl("^ann1\\.Glyma", all_genes) &
+    grepl(
+      "^ann1\\.Glyma",
+      all_genes
+    ) &
       !all_genes %in% organelle_genes
   ]
   
   scn_genes <- all_genes[
-    grepl("^Hetgly", all_genes) &
+    grepl(
+      "^Hetgly",
+      all_genes
+    ) &
       !all_genes %in% organelle_genes
   ]
   
@@ -537,7 +426,10 @@ for (i in seq_along(samples)) {
   )
   
   cells_keep <- colnames(obj)[
-    obj$species %in% c("soybean", "scn")
+    obj$species %in% c(
+      "soybean",
+      "scn"
+    )
   ]
   
   filtered_obj <- subset(
@@ -586,7 +478,7 @@ cell_number_after_species_filter <- sapply(
 )
 
 ###############################################################################
-## 9. Merge all filtered samples
+## 8. Merge all filtered samples
 ###############################################################################
 
 scn_combine_filter <- merge(
@@ -613,26 +505,9 @@ scn_combine_filter$nFeature_RNA <- Matrix::colSums(
   count_mat > 0
 )
 
-###############################################################################
-## 10. Export compact filtering summary
-###############################################################################
-
-filtering_cell_number_summary <- data.frame(
-  sample = samples,
-  raw_cells = as.integer(raw_cell_number),
-  cells_after_nCount_nFeature_filter = as.integer(cell_number_after_basic_filter),
-  cells_after_doublet_filter = as.integer(cell_number_after_doublet),
-  cells_after_species_filter = as.integer(cell_number_after_species_filter)
-)
-
-write.csv(
-  filtering_cell_number_summary,
-  file = file.path(output_dir, "filtering_cell_number_summary.csv"),
-  row.names = FALSE
-)
 
 ###############################################################################
-## 11. Inspect final QC metrics
+## 9. Inspect final QC metrics
 ###############################################################################
 
 p <- VlnPlot(
@@ -694,7 +569,7 @@ ggsave(
 )
 
 ###############################################################################
-## 12. Normalize, identify variable features, scale data, and run PCA
+## 10. Normalize, identify variable features, scale data, and run PCA
 ###############################################################################
 
 scn_combine_filter <- NormalizeData(
@@ -733,7 +608,7 @@ print(
 dev.off()
 
 ###############################################################################
-## 13. Save filtered objects
+## 11. Save filtered objects
 ###############################################################################
 
 saveRDS(
